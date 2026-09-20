58335f6c77b92782c05069cebfd389f733e85713
sonu sharma <dexterthebatman00@gmail.com>
2026-09-20 21:53:34 +0530

    attach search button and dom

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+async function getWeather(city) {
+    try {
+    const apiKey = "RNG38D3GJ9JZWPFSKXHVURT6D";
+    const apiURL  = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
-//fetch weather
-function getWeather() {
-    fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/africa?unitGroup=us&key=J7SAH98DCRRNQ6HXQUXXNR7VH&contentType=json')//weather API
-    .then((response) => {
-        return response.json();
-    }).then((response) => {
-        console.log(response);
-    }).catch((err) => { 
+    const weather = await fetch(apiURL + city + `?key=${apiKey}`);
+    const weatherResponse = await weather.json();
+    
+    if(weatherResponse) {
+        document.getElementById('address').innerHTML = `location ${weatherResponse.address}`;
+        document.getElementById('temp').innerHTML = Math.round(weatherResponse.currentConditions.temp) + '°F';
+        document.getElementById('humidity').innerHTML = weatherResponse.currentConditions.humidity;
+        document.getElementById('conditions').innerHTML = weatherResponse.currentConditions.conditions;
+        document.getElementById('wind').innerHTML = weatherResponse.currentConditions.windspeed;
+
+    } else {
+        alert("City not found!");
+    }
+
+    console.log(weatherResponse)
+
+    }catch(err) {
         console.log(err);
-    })
+    }
+
 }
 
-getWeather();
 
+//attach event listener to search button
+document.getElementById('searchBtn').addEventListener("click", () => {
+    const city = document.getElementById("weather").value;
+    getWeather(city);
+})
